977fd340146fb982b14074d435471a6dbd5a6b54
Foysalmeazi <39994598+Foysalmeazi@users.noreply.github.com>
2025-07-04 07:46:53 +0000

    Operations

---
+++ 01_Basics/03_Operations.js
@@ -0,0 +1,16 @@
+
+let num = 10;
+for (let i = 1; i<=num;++i)
+{
+    console.log(i);
+    
+}
+
+
+let x=3;
+let result = x++;
+console.log(result);
+
+let y =3;
+let result2 = ++y;
+console.log(result2);
