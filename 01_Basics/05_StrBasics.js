b808b440ea975585ef968f7c0a5afc119c38f138
Foysalmeazi <39994598+Foysalmeazi@users.noreply.github.com>
2025-07-07 04:00:50 +0000

    New Changes

--- 01_Basics/05_StrBasics.js
+++ 01_Basics/05_StrBasics.js
@@ -1,1 +1,23 @@
-console.log("Testing Git Push");
+
+const str1 = "Abdullah";
+const str2 = "Al Foysal";
+
+const age =20;
+const address = 'I live in Australia';
+
+//console.log(`My name is ${str1} ${str2}, Age: ${age} and ${address}`);
+
+const strname = new String ('Foysal Abdullah');
+
+const length = strname.length;
+
+const splitStr = strname.split(' ');
+console.log(splitStr);
+
+const firstName = splitStr[0];
+const lastName = splitStr[1];
+const charIs = strname.charAt(9);
+
+
+
+console.log(`The length of this String is ${length} and the first name is ${firstName} and the 9th index char is ${charIs} `);
