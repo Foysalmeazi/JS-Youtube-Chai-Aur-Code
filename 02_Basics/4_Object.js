62cd34b16bebf7eaad7072a06ea02d9e2475a78c
Foysalmeazi <39994598+Foysalmeazi@users.noreply.github.com>
2025-08-10 11:57:48 +0000

    Object Destructuring example

--- 02_Basics/4_Object.js
+++ 02_Basics/4_Object.js
@@ -1,66 +1,64 @@
-
-//const user  = new Object();
-
+// Create an empty object fbUser1 and add properties
 const fbUser1 = {};
+fbUser1.name = 'Hamid';
+fbUser1.email = 'hamid@google.com';
+fbUser1.mobile = '013456789087';
 
-fbUser1.name = 'Hamid',
-fbUser1.email = 'hamid@google.com',
-fbUser1.mobile = '013456789087'
-
+// Create another empty object fbUser2 and add properties
 const fbUser2 = {};
+fbUser2.age = 20;
+fbUser2.height = '5:7';
 
-fbUser2.age = 20,
-fbUser2.height = '5:7'
-
-const allUser = { ...fbUser1, ...fbUser2 }
+// Merge fbUser1 and fbUser2 into a new object allUser using spread operator
+const allUser = { ...fbUser1, ...fbUser2 };
 console.log('All User Details:\n', allUser);
 
 
-const obj3 =
-{
+// Define object obj3 with user details
+const obj3 = {
     name: 'Rahul',
     age: 20,
     country: 'Bangladesh'
-}
-const obj4 =
-{
+};
+
+// Define object obj4 with brother’s details
+const obj4 = {
     breakrother: 'Ramim',
     brotherAge: 24,
     brotherCountry: 'India'
-}
-//const newObj = Object.assign({}, obj3,obj4);
-const nextObj = { label: 'Total objects are:',
+};
+
+// Merge obj3 and obj4 into nextObj with an additional label property
+const nextObj = { 
+    label: 'Total objects are:',
     ...obj3, 
-    ...obj4 }
+    ...obj4 
+};
 console.log(nextObj);
 
 
-const userprofile =
-{
+// Nested object userprofile with deep properties
+const userprofile = {
     profileName: 'Rahumin',
-    fullname:
-    {
-        userFullname:
-        {
+    fullname: {
+        userFullname: {
             fisrtName: 'Rahumin',
             lastName: 'Hossain'
         }
-
     }
-}
+};
 
-console.log('UserProfile:\n',userprofile);
+console.log('UserProfile:\n', userprofile);
 
 
-// Object De-Structuring
-
-const course = 
-{
+// Object Destructuring example from course object
+const course = {
     courseName: 'English',
     price: 1000,
     instructor: 'Saifurs English'
-}
+};
 
-const {instructor: instract} = course;
+// Extract 'instructor' property and rename it to 'instract'
+const { instructor: instract } = course;
 
 console.log('Course Instructor is: ', instract);
