d762fcfbefb54654c6c1e4a83bb6769dc68205a6
Foysalmeazi <39994598+Foysalmeazi@users.noreply.github.com>
2025-08-17 10:30:05 +0000

    Others Loop

---
+++ 05_Iterations/02_otherLoop.js
@@ -0,0 +1,35 @@
+
+// while loop
+
+let i=2;
+
+while(i<=10)
+{
+    console.log(`The value of i is ${i}`);
+    i=i+2;
+}
+
+const array = [2,10,14,15,16,17,18];
+
+console.log('\n');
+
+let arr = 0;
+
+while(arr<array.length)
+{
+    console.log(`The value of array is ${array[arr]}`);
+    arr++;
+    
+}
+
+
+const newArray = [24,45,67,89,90];
+
+let j=0;
+
+do
+{
+    console.log(`The values: ${newArray[j]}`);
+    j++;
+    
+} while(j<newArray.length);
