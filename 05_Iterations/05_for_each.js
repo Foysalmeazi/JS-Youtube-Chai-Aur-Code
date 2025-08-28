9eb4df1009358c9cf52d9f8aa86d59c1107143e4
Foysalmeazi <39994598+Foysalmeazi@users.noreply.github.com>
2025-08-28 22:31:34 +0000

    for each loop

--- 05_Iterations/05_for_each.js
+++ 05_Iterations/05_for_each.js
@@ -8,7 +8,7 @@ const programming = ['JS', 'Java', 'Ruby', 'CPP', 'Swift'];
 
 function printMe(item)
 {
-    console.log(item);
+    //console.log(item);
     
 }
 programming.forEach(printMe)
@@ -18,4 +18,35 @@ programming.forEach((item,index,array)=>
 {
 //console.log(`Value: ${item}, index ${index}, Array ${array}`);
 
+})
+
+
+const holliwoodMovies = [
+
+   {
+    name: 'Shawshank Redumptionm',
+    year: 1994,
+    director: "Frank Darabont",
+    genre: "Drama"
+  },
+  {
+    name: 'Night Club',
+    year: 2008,
+    director: "Christopher Nolan",
+    genre: "Action"
+  },
+  {
+    name: 'Chis Movie',
+    year: 2010,
+    director: "Christopher Nolan",
+    genre: "Sci-Fi"
+  },
+
+];
+
+holliwoodMovies.forEach((mvitem)=>
+{
+    console.log('\n');
+    console.log(`Movies Name: ${mvitem.name}\nDirector: ${holliwoodMovies.director}\nGenre: ${holliwoodMovies.genre}`);
+    
 })
